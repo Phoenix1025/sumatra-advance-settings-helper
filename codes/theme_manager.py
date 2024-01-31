fb89d9a00e03457dc43e189e0c2a76a61b81ceb6
Kenneth <kennethalisen433@protonmail.com>
2024-01-31 16:16:03 +0800

    theme_manager.py - Introduced CustomThemeDialog class to handle customization, added a settings database to save settings data.

--- codes/theme_manager.py
+++ codes/theme_manager.py
@@ -1,32 +1,12 @@
 import re
+import json
 import tkinter as tk
-from enum import Enum
 from tkinter import ttk
 from pathlib import Path
+from tkinter import colorchooser, messagebox
 
-
-class Themes(Enum):
-    LIGHT = {
-        "TextColor": "#000000",
-        "BackgroundColor": "#ffffff",
-        "MainWindowBackground": "#80fff200",
-        "SelectionColor": "#f5fc0c",
-        "GradientColors": "#2828aa #28aa28 #aa2828",
-    }
-    DARK = {
-        "TextColor": "#dddddd",
-        "BackgroundColor": "#111111",
-        "MainWindowBackground": "#2e2e2e",
-        "SelectionColor": "#f5fc0c",
-        "GradientColors": "#2828aa #28aa28 #aa2828",
-    }
-    DRACULA = {
-        "TextColor": "#f8f8f2",
-        "BackgroundColor": "#21222c",
-        "MainWindowBackground": "#282a36",
-        "SelectionColor": "#f4f4f4",
-        "GradientColors": "#2828aa #28aa28 #aa2828",
-    }
+SETTINGS_FOLDER = Path('~/AppData/Local/SumatraPDF').expanduser()
+THEME_APP_SIZE = '200x50'
 
 
 class SumatraThemeException(Exception):
@@ -42,18 +22,26 @@ class SumatraThemeSetting:
         "GradientColors",  # Must be added in advanced settings first.
     ]
 
-    def __init__(self, settings_file, theme_source):
+    def __init__(self, settings_file, settings_db):
         self.settings_file = settings_file
-        self.theme_source = theme_source
+        self._settings_db = settings_db
 
     @property
     def available_themes(self):
-        available_themes = {
-            "Light": self.theme_source.LIGHT.value,
-            "Dark": self.theme_source.DARK.value,
-            "Dracula": self.theme_source.DRACULA.value,
-        }
-        return available_themes
+        return self._get_database()['themes']
+
+    def _get_database(self):
+        with open(self._settings_db, 'r') as db:
+            return json.load(db)
+
+    def add_theme(self, new_theme_color, new_theme):
+        database = self._get_database()
+        database['themes'][new_theme_color] = new_theme
+        self.update_db(database)
+
+    def update_db(self, data):
+        with open(self._settings_db, 'w') as db:
+            json.dump(data, db, indent=4)
 
     def get_current_theme(self):
         current_theme = {}
@@ -71,11 +59,13 @@ class SumatraThemeSetting:
                     raise SumatraThemeException(f'No color found for {color_setting} in settings.') from e
         return current_theme
 
-    def change_theme(self, new_theme):
+    def change_theme(self, theme_color=None):
+        if not theme_color:
+            return
+        new_theme = self.available_themes.get(theme_color)
         if not new_theme:
             return
         current_theme = self.get_current_theme()
-
         updated_theme = []
         with open(self.settings_file, "r+") as f:
             for line in f.readlines():
@@ -84,51 +74,131 @@ class SumatraThemeSetting:
                     new_color = new_theme[color_setting]
                     line = re.sub(current_color, new_color, line)
                 updated_theme.append(line)
-
             f.seek(0)
             f.writelines(updated_theme)
             f.truncate()
 
 
-APP_SIZE = '250x80'
+class CustomThemeDialog(tk.Toplevel):
+    def __init__(self, master, current_theme, apply_callback):
+        super().__init__(master)
+        self.title("Custom Theme")
+        self.geometry("390x200")
+
+        self.current_theme = current_theme
+        self.color_entries = []
+
+        self.color_pick_label = ttk.Label(self, text='Color picker')
+        self.color_radio = ttk.Radiobutton(self, command=self.pick_color, state=tk.DISABLED)
+
+        self.color_pick_label.grid(row=0, column=3)
+        self.color_radio.grid(row=0, column=4)
+
+        for i, color_setting in enumerate(SumatraThemeSetting.COLOR_SETTINGS):
+            setting_label = ttk.Label(self, text=f"{color_setting}:")
+            entry = ttk.Entry(self)
+            entry.bind("<FocusIn>", lambda event, entry=entry: self.enable_radio(entry))
+
+            entry.insert(0, current_theme.get(color_setting, ""))
+
+            setting_label.grid(row=i, column=0, padx=5, pady=5, sticky="e")
+            entry.grid(row=i, column=1, padx=5, pady=5, sticky="w")
+
+            self.color_entries.append(entry)
+
+        apply_button = ttk.Button(self, text="Apply", command=self.apply_custom_theme)
+        apply_button.grid(row=len(SumatraThemeSetting.COLOR_SETTINGS), column=0, columnspan=2, pady=10)
+
+        self.apply_callback = apply_callback
+
+    def apply_custom_theme(self):
+        custom_theme = {}
+        for i, color_setting in enumerate(SumatraThemeSetting.COLOR_SETTINGS):
+            custom_theme[color_setting] = self.color_entries[i].get()
+
+        self.apply_callback(custom_theme)
+        self.destroy()
+
+    def enable_radio(self, entry):
+        self.color_radio.configure(state=tk.NORMAL)
+        self.color_entry = entry
+
+    def pick_color(self):
+        if self.color_entry == self.color_entries[-1]:
+            new_gradient_colors = []
+            for i in range(3):
+                color = colorchooser.askcolor(title=f'Pick Gradient Color {i+1}')
+                if color[1] is not None:
+                    new_gradient_colors.append(color[1])
+
+            new_gradient_colors = ' '.join(new_gradient_colors)
+            self.color_entry.delete(0, tk.END)
+            self.color_entry.insert(0, new_gradient_colors)
+        else:
+            color = colorchooser.askcolor(title='Pick a color')
+            self.color_radio.configure(state=tk.DISABLED)
+            if color[1] is not None:
+                self.color_entry.delete(0, tk.END)
+                self.color_entry.insert(0, color[1])
 
 
 class SumatraThemeApp:
     def __init__(self, master, theme_setting):
+        # window setup
         self.master = master
         self.theme_setting = theme_setting
-        self.master.geometry(APP_SIZE)
+        self.master.geometry(THEME_APP_SIZE)
         self.master.resizable(0, 0)
         self.master.title('Sumatra Advance Settings')
 
-        self.new_theme_var = tk.StringVar()
+        self.theme_color_string = tk.StringVar()
 
-        # Theme buttons
-        self.dark_theme_button = ttk.Button(text='Dark', command=lambda: self.get_theme('Dark'))
-        self.dark_theme_button.grid(row=0, column=0, padx=5, pady=5)
-        self.default_theme_button = ttk.Button(text='Light', command=lambda: self.get_theme('Light'))
-        self.default_theme_button.grid(row=0, column=1)
-        self.dracula_theme_button = ttk.Button(text='Dracula', command=lambda: self.get_theme('Dracula'))
-        self.dracula_theme_button.grid(row=0, column=2, padx=5, pady=5)
+        # Theme options
+        self.all_themes = list(self.theme_setting.available_themes)
+        self.theme_options_label = ttk.Label(text='Choose Theme:', font='Arial 10 bold')
+        self.theme_option_menu = ttk.OptionMenu(
+            self.master,
+            self.theme_color_string,
+            'Themes',
+            *(self.all_themes),
+            direction='flush',
+            command=self.apply_theme
+        )
+        self.theme_options_label.grid(row=0, column=0, pady=5)
+        self.theme_option_menu.grid(row=0, column=1, padx=5, pady=5)
 
-        # Apply theme button
-        self.apply_theme_button = ttk.Button(text='Apply', command=self.apply_theme)
-        self.apply_theme_button.grid(row=3, column=1, pady=10)
+    def apply_theme(self, theme_color):
+        if theme_color == 'Custom':
+            self.show_custom_theme_dialog()
+            return
+        try:
+            self.theme_setting.change_theme(theme_color)
+        except SumatraThemeException as e:
+            self.display_error_message(e)
 
-    def get_theme(self, theme_color):
-        self.new_theme_var.set(theme_color)
-        return self.theme_setting.available_themes.get(theme_color)
+    def show_custom_theme_dialog(self):
+        current_theme = self.theme_setting.get_current_theme()
+        custom_dialog = CustomThemeDialog(self.master, current_theme, self.apply_custom_theme)
+        self.master.wait_window(custom_dialog)
 
-    def apply_theme(self):
-        new_theme = self.get_theme(self.new_theme_var.get())
-        self.theme_setting.change_theme(new_theme)
+    def apply_custom_theme(self, custom_theme):
+        try:
+            self.theme_setting.add_theme('Custom', custom_theme)
+            self.theme_setting.change_theme('Custom')
+        except SumatraThemeException as e:
+            self.display_error_message(e)
+
+    def display_error_message(self, message):
+        messagebox.showinfo("An error occured", message)
 
 
 def main():
-    settings_file = Path("~/Appdata/Local/SumatraPDF/SumatraPDF-settings.txt").expanduser()
-    theme_setting = SumatraThemeSetting(settings_file, Themes)
-    root = tk.Tk()
-    app = SumatraThemeApp(root, theme_setting)
+    settings_file = SETTINGS_FOLDER / 'SumatraPDF-settings.txt'
+    settings_db = SETTINGS_FOLDER / 'backup_db.json'
+
+    theme_setting = SumatraThemeSetting(settings_file, settings_db)
+
+    app = SumatraThemeApp(tk.Tk(), theme_setting)
     app.master.mainloop()
 
 
